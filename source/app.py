b3c272f903e83c80fa04cf7a71c85a1e9cd500ff
FiodorSuslionkov <44852952+FiodorSuslionkov@users.noreply.github.com>
2018-11-27 02:13:28 +0300

    Delete app.py

--- source/app.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from source.graphics.paint import *
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    Window = MainWindow()
-    sys.exit(app.exec_())
